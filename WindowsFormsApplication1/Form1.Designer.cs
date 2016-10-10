e30b425276c9a3b4cbee1b190b3b7ede33f58544
tingfeng <1770963469@qq.com>
2016-10-10 15:37:24 +0800

    submit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Xiaogongju
+{
+    partial class Xiaogongju
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(10, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(598, 334);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(590, 308);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "获取验证码";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(74, 145);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(396, 129);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(113, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 64);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "获取验证码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(590, 308);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "识别验证码";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(590, 308);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "获取数据";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Xiaogongju
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(604, 344);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Xiaogongju";
+            this.Text = "小工具";
+            this.Load += new System.EventHandler(this.Xiaogongju_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Xiaogongju
+{
+    public partial class Xiaogongju : Form
+    {
+        HttpClient web;
+        public Byte[] codeImageData;
+        public Xiaogongju()
+        {
+            InitializeComponent();
+        }
+
+        private void Xiaogongju_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged() { 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            string URL = "http://gsxt.gzgs.gov.cn/search!generateCode.shtml";
+            URL += "?validTag=searchImageCode";
+            web = new HttpClient();
+            try {
+                codeImageData = web.DownloadData(URL);
+                pictureBox1.Image = Bitmap.FromStream(new MemoryStream(codeImageData));
+                this.ocr_code();
+            }catch (WebException err){
+                MessageBox.Show(err.InnerException.Message);
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string url = "http://gsxt.gzgs.gov.cn/query!searchSczt.shtml";
+            StringBuilder postData = new StringBuilder();
+            postData.Append("q=1&validCode=" + textBox1.Text);
+            byte[] sendData = Encoding.GetEncoding("UTF-8")
+                                .GetBytes(postData.ToString());
+            web.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            web.Headers.Add("ContentLength", sendData.Length.ToString());
+            byte[] recData = web.UploadData(url, "POST", sendData);
+            MessageBox.Show(Encoding.GetEncoding("UTF-8").GetString(recData));
+        }
+
+        private void ocr_code()
+        {
+            string url = "http://apis.baidu.com/idl_baidu/baiduocrpay/idlocrpaid";
+            StringBuilder postData = new StringBuilder();
+            postData.Append("fromdevice=pc&clientip=10.10.10.0&detecttype=LocateRecognize"
+                            +"&languagetype=CHN_ENG&imagetype=1");
+            string base64String = Convert.ToBase64String(codeImageData);
+            Console.Write(base64String);
+            postData.Append("&image=" + base64String);
+            byte[] sendData = Encoding.GetEncoding("UTF-8")
+                                .GetBytes(postData.ToString());
+            web.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            web.Headers.Add("ContentLength", sendData.Length.ToString());
+            web.Headers.Add("apikey", "key");
+            byte[] recData = web.UploadData(url, "POST", sendData);
+            //上传完成
+            //获取json数据
+            string jsonString;
+            textBox2.Text = 
+                jsonString = Encoding.GetEncoding("UTF-8")
+                    .GetString(recData);
+            //MessageBox.Show(jsonString);
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A)
+            {
+                ((TextBox)sender).SelectAll();
+            } 
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class HttpClient : WebClient
+    {
+        // Cookie 容器
+        private CookieContainer cookieContainer;
+
+        /**/
+        /// <summary>
+        /// 创建一个新的 WebClient 实例。
+        /// </summary>
+        public HttpClient()
+        {
+            this.cookieContainer = new CookieContainer();
+        }
+
+        /**/
+        /// <summary>
+        /// 创建一个新的 WebClient 实例。
+        /// </summary>
+        /// <param name="cookie">Cookie 容器</param>
+        public HttpClient(CookieContainer cookies)
+        {
+            this.cookieContainer = cookies;
+        }
+
+        /**/
+        /// <summary>
+        /// Cookie 容器
+        /// </summary>
+        public CookieContainer Cookies
+        {
+            get { return this.cookieContainer; }
+            set { this.cookieContainer = value; }
+        }
+
+        /**/
+        /// <summary>
+        /// 返回带有 Cookie 的 HttpWebRequest。
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                HttpWebRequest httpRequest = request as HttpWebRequest;
+                httpRequest.CookieContainer = cookieContainer;
+            }
+            return request;
+        }
+    }
+}
